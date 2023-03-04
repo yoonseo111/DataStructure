78388daa39d6852cae516045e884d3db15710cb9
yoonseo111 <126842110+yoonseo111@users.noreply.github.com>
2023-03-04 22:31:57 +0900

    Week 01

---
+++ 백준 1152.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int result = 0;
+	string word;
+	getline(cin, word);
+
+	result = 1;
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (word[i] == ' ') result++;
+	}
+
+	if (word[0] == ' ')
+		result--;
+	if (word[word.length() - 1] == ' ')
+		result--;
+
+	cout << result << '\n';
+	return 0;
+}
